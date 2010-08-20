461f3d4b05befa77a7ec89c7e08e462170495436
Wiehann Radyn <wradyn@gmail.com>
2010-08-20 22:00:53 +0200

    Added some more quotes.

--- lib/rumi.rb
+++ lib/rumi.rb
@@ -8,7 +8,11 @@ module Rumi
               "Thirst drove me down to the water where I drank the moon's reflection.",
               "Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.",
               "Every object, every being, is a jar full of delight.",
-              "Let the beauty we love be what we do."]
+              "Let the beauty we love be what we do.",
+              "In silence there is eloquence. Stop weaving and watch how the pattern improves.",
+              "Observe the wonders as they occur around you. Don't claim them. Feel the artistry moving through and be silent.",
+              "You were born with wings. Why prefer to crawl through life?",
+              "Everyone is so afraid of death, but the real sufis just laugh: nothing tyrannizes their hearts. What strikes the oyster shell does not damage the pearl."]
     puts quotes[rand(quotes.size)]
   end
 end
